4e1d1f964960ab87cff4f726d5c7876c0cf19a72
NikPeg <peganov.nik@gmail.com>
2025-04-07 22:30:15 +0300

    from

--- proto/renderer_pb2_grpc.py
+++ proto/renderer_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import .renderer_pb2 as renderer__pb2
+from . import renderer_pb2 as renderer__pb2
 
 GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
